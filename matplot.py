94010984f5294ab80e8b10d7c23fdf0e228258f0
Nishant Kumar <nishantkumar6531007@gmail.com>
2018-08-24 22:59:41 +0530

    plotting using python
    Working With Python Using Matplot library ..

---
+++ matplot.py
@@ -0,0 +1,9 @@
+year={1990,1991,1992,1993,1994,1995}
+pop={2,3,4,5,6,7}
+import matplotlib.pyplot as plt
+
+# Make a line plot: year on the x-axis, pop on the y-axis
+plt.plot(year,pop)
+
+# Display the plot with plt.show()
+plt.show()
