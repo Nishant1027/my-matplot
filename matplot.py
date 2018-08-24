3e509ba28454f2d54135056927df28757635b51a
Nishant Kumar <35365565+Nishant1027@users.noreply.github.com>
2018-08-24 23:16:05 +0530

    Update matplot.py

--- matplot.py
+++ matplot.py
@@ -1,5 +1,5 @@
-year={1990,1991,1992,1993,1994,1995}
-pop={2,3,4,5,6,7}
+year=[1990,1991,1992,1993,1994,1995]
+pop=[2,3,4,5,6,7]
 import matplotlib.pyplot as plt
 
 # Make a line plot: year on the x-axis, pop on the y-axis
